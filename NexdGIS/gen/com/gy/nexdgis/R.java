edbcef17a453347db8517a6c572c43e10c90cd42
Pepgo <pepgozcy@gmail.com>
2014-05-04 12:16:24 +0800

    Merge branch 'shiotoli'
    Conflicts:
    NexdGIS/AndroidManifest.xml
    NexdGIS/bin/AndroidManifest.xml
    NexdGIS/bin/TestActivity.apk
    NexdGIS/bin/classes.dex
    NexdGIS/bin/classes/com/gy/nexdgis/R$id.class
    NexdGIS/bin/classes/com/gy/nexdgis/R$layout.class
    NexdGIS/bin/classes/com/gy/nexdgis/R$menu.class
    NexdGIS/bin/classes/com/gy/nexdgis/R$string.class
    NexdGIS/bin/classes/com/gy/nexdgis/R$style.class
    NexdGIS/bin/classes/com/nexdgis/BuildingActivity$1.class
    NexdGIS/bin/classes/com/nexdgis/BuildingActivity$2.class
    NexdGIS/bin/classes/com/nexdgis/BuildingActivity$3.class
    NexdGIS/bin/classes/com/nexdgis/BuildingActivity$4.class
    NexdGIS/bin/classes/com/nexdgis/BuildingActivity$5.class
    NexdGIS/bin/classes/com/nexdgis/BuildingActivity.class
    NexdGIS/bin/classes/com/nexdgis/NexdMap$1.class
    NexdGIS/bin/classes/com/nexdgis/NexdMap$2.class
    NexdGIS/bin/classes/com/nexdgis/NexdMap$NexdMapHandler.class
    NexdGIS/bin/classes/com/nexdgis/NexdMap.class
    NexdGIS/bin/classes/com/nexdgis/TestActivity.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$1.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$2.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$3$1.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$3.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$4.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$5.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$6$1.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$6.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$7.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$8$1.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine$8.class
    NexdGIS/bin/classes/com/nexdgis/remote/NexdEngine.class
    NexdGIS/bin/resources.ap_
    NexdGIS/gen/com/gy/nexdgis/R.java
    NexdGIS/res/layout/building_activity.xml
    NexdGIS/res/layout/nexd_map.xml
    NexdGIS/src/com/nexdgis/BuildingActivity.java
    NexdGIS/src/com/nexdgis/NexdMap.java
    NexdGIS/src/com/nexdgis/remote/NexdEngine.java

--- NexdGIS/gen/com/gy/nexdgis/R.java
+++ NexdGIS/gen/com/gy/nexdgis/R.java
@@ -24,12 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+<<<<<<< HEAD
         public static final int action_settings=0x7f080005;
         public static final int button=0x7f080003;
         public static final int floorlistview=0x7f080001;
         public static final int listview=0x7f080004;
         public static final int nexd_map=0x7f080000;
         public static final int text_view=0x7f080002;
+=======
+        public static final int action_settings=0x7f080006;
+        public static final int button=0x7f080004;
+        public static final int button1=0x7f080002;
+        public static final int floorlistview=0x7f080001;
+        public static final int listview=0x7f080005;
+        public static final int nexd_map=0x7f080000;
+        public static final int text_view=0x7f080003;
+>>>>>>> shiotoli
     }
     public static final class layout {
         public static final int building_activity=0x7f030000;
